473fb77fdcb270e2727eb5467959c53d85bf0387
Atha Ardisa <ardisaatha1@gmail.com>
2022-11-28 15:48:05 +0700

    add routing for login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import LandingPage from './components/content/LandingPage';
+import Login from './components/content/Login';
 import Register from './components/content/Register';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
