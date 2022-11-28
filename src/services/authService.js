6763a9af7ceb106dea694a2d3f612808bcc62c67
Atha Ardisa <ardisaatha1@gmail.com>
2022-11-28 15:49:13 +0700

    add service for login from backend

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,17 +1,17 @@
 import API from './api';
 
 const AuthService = {
-//   login: async function (data) {
-//     try {
-//       const response = await API.post('/auth/login', data);
-//       API.defaults.headers['Authorization'] = `Bearer ${response.token}`;
-//       setHeadersAndStorage(response.data);
-//       return response;
-//     } catch (err) {
-//       console.log('Auth service error', err);
-//       throw err;
-//     }
-//   },
+  login: async function (data) {
+    try {
+      const response = await API.post('/api/auth/login', data);
+      API.defaults.headers['Authorization'] = `Bearer ${response.data.data.accessToken}`;
+      setHeadersAndStorage(response.data.data);
+      return response;
+    } catch (err) {
+      console.log('Auth service error', err);
+      throw err;
+    }
+  },
 
   register: async function (data) {
     try {
