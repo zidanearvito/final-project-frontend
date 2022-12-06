c8302448acebc96dd60eebfedf7a6c162cd14ada
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-06 20:17:16 +0700

    make routing for login admin and dashboard admin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import DashboardAdmin from './components/content/DashboardAdmin';
 import LandingPage from './components/content/LandingPage';
 import Login from './components/content/Login';
+import LoginAdmin from './components/content/LoginAdmin';
 import Register from './components/content/Register';
 
 function App() {
@@ -10,8 +12,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard/admin" element={<DashboardAdmin />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/login/admin" element={<LoginAdmin />} />
       </Routes>
     </BrowserRouter>
   );
