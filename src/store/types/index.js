009294ac4c306b026829941c98b95ac76e6ff02e
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-13 21:50:16 +0700

    add type for getUser, and updateProfile

--- src/store/types/index.js
+++ src/store/types/index.js
@@ -3,3 +3,4 @@ export const LOGIN_ADMIN = "LOGIN_ADMIN";
 export const REGISTER = "REGISTER";
 export const LOGOUT = "LOGOUT";
 export const UPDATE_PROFILE = "UPDATE_PROFILE";
+export const GET_USER = "GET_USER";
