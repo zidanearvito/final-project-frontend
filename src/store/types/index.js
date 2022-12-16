183965754da1f15ad3d65b82f4864f2c1295dc3c
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-16 18:49:22 +0700

    add type

--- src/store/types/index.js
+++ src/store/types/index.js
@@ -4,3 +4,9 @@ export const REGISTER = "REGISTER";
 export const LOGOUT = "LOGOUT";
 export const UPDATE_PROFILE = "UPDATE_PROFILE";
 export const GET_USER = "GET_USER";
+export const GET_AIRPORT = "GET_AIRPORT"
+export const CREATE_AIRPORT = "CREATE_AIRPORT"
+export const GET_AIRPORT_ID = "GET_AIRPORT_ID"
+export const UPDATE_AIRPORT = "UPDATE_AIRPORT"
+export const DELETE_AIRPORT = "DELETE_AIRPORT"
+
