5f4f3cdfbeebb61eceab7a48c52db7e85256ab86
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-16 18:51:38 +0700

    combine airport reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth";
+import airportReducer from "./airport";
 
 export default combineReducers({
-    authReducer,
+    authReducer, airportReducer
 })
