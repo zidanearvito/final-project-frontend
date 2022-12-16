a103eb421a9f47097ab0326bb9c220b3c9e8d24d
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-16 18:58:16 +0700

    create airport redux reducer

---
+++ src/store/reducers/airport.js
@@ -0,0 +1,51 @@
+import {
+  CREATE_AIRPORT,
+  DELETE_AIRPORT,
+  GET_AIRPORT,
+  GET_AIRPORT_ID,
+  UPDATE_AIRPORT,
+} from "../types";
+
+const initialState = {
+  data: [],
+  dataById: false,
+};
+
+const airportReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_AIRPORT:
+      return {
+        ...state,
+        data: payload.data,
+      };
+
+    case CREATE_AIRPORT:
+      return {
+        ...state,
+        data: payload,
+      };
+    case GET_AIRPORT_ID:
+      return {
+        ...state,
+        dataById: payload.data,
+      };
+
+    case UPDATE_AIRPORT:
+      return {
+        ...state,
+        dataById: payload.data,
+      };
+
+    case DELETE_AIRPORT:
+      return {
+        ...state,
+        dataById: payload.data,
+      };
+    default:
+      return state;
+  }
+};
+
+export default airportReducer;
