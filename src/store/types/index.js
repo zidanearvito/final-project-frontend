914b6d6b15e43230d9be48eb128e9ddffbbe5802
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-17 14:25:52 +0700

    add type for CRUD company

--- src/store/types/index.js
+++ src/store/types/index.js
@@ -1,12 +1,20 @@
+// user
 export const LOGIN = "LOGIN";
 export const LOGIN_ADMIN = "LOGIN_ADMIN";
 export const REGISTER = "REGISTER";
 export const LOGOUT = "LOGOUT";
 export const UPDATE_PROFILE = "UPDATE_PROFILE";
 export const GET_USER = "GET_USER";
+// airport
 export const GET_AIRPORT = "GET_AIRPORT"
 export const CREATE_AIRPORT = "CREATE_AIRPORT"
 export const GET_AIRPORT_ID = "GET_AIRPORT_ID"
 export const UPDATE_AIRPORT = "UPDATE_AIRPORT"
 export const DELETE_AIRPORT = "DELETE_AIRPORT"
+// company
+export const GET_COMPANY = "GET_COMPANY"
+export const CREATE_COMPANY = "CREATE_COMPANY"
+export const GET_COMPANY_ID = "GET_COMPANY_ID"
+export const UPDATE_COMPANY = "UPDATE_COMPANY"
+export const DELETE_COMPANY = "DELETE_COMPANY"
 
