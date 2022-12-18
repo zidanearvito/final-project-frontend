dfc9bc21f75bc01a0ff0c0f8eaeb143b09fddbd3
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-18 15:58:39 +0700

    combine airplane reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,7 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./auth";
 import airportReducer from "./airport";
 import companyReducer from "./company";
+import airplaneReducer from "./airplane";
 
 export default combineReducers({
-    authReducer, airportReducer, companyReducer
-})
+  authReducer,
+  airportReducer,
+  companyReducer,
+  airplaneReducer,
+});
