8d088ee34eddf5aac08f6eb12114f8a5423aeaba
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-19 10:18:53 +0700

    integration with backend API

---
+++ src/services/ticketService.js
@@ -0,0 +1,57 @@
+import API from "./api";
+
+const TicketService = {
+  getTicket: async function (data) {
+    try {
+      const response = await API.get("/api/ticket", data);
+      // console.log(response)
+      return response;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
+
+  createTicket: async function (data) {
+    try {
+      const response = await API.post("/api/ticket/create", data);
+      return response;
+    } catch (error) {
+      console.log("Ticket service error", error);
+      throw error;
+    }
+  },
+
+  getTicketById: async function (id) {
+    try {
+      const response = await API.get(`/api/ticket/${id}`);
+      return response;
+    } catch (err) {
+      console.log("Ticket service error", err);
+      throw err;
+    }
+  },
+
+  updateTicket: async function (id, data) {
+    try {
+      const response = await API.put(`/api/ticket/update/${id}`, data);
+      // console.log(response)
+      return response;
+    } catch (error) {
+      console.log("Ticket service error", error);
+      throw error;
+    }
+  },
+
+  deleteTicket: async function (id) {
+    try {
+      const response = await API.delete(`/api/ticket/delete/${id}`);
+      return response;
+    } catch (err) {
+      console.log("Ticket service error", err);
+      throw err;
+    }
+  },
+};
+
+export default TicketService;
