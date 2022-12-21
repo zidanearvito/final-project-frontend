6903a8ae96aae87217855cb78abbd9c32646b60d
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-21 10:37:43 +0700

    add routing for search data and input passenger

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Profile from "./components/Profile/Index";
 import Airport from "./components/content/Airport";
 import Airplane from "./components/content/Airplane";
 import Ticket from "./components/content/Ticket";
+import SearchIndex from "./components/Buyer/searchCard/SearchIndex";
+import InputIndex from "./components/Buyer/InputData/InputIndex";
 
 function App() {
   return (
@@ -26,6 +28,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/login/admin" element={<LoginAdmin />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<SearchIndex />} />
+        <Route path="/inputData" element={<InputIndex />} />
       </Routes>
     </BrowserRouter>
   );
