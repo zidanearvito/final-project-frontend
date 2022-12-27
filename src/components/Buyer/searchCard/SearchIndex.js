c87afe026ecd3cd5235fff88bd89c6b0c5945bb9
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-28 00:22:07 +0700

    add validation if user did not logged in then will navigate to login page

--- src/components/Buyer/searchCard/SearchIndex.js
+++ src/components/Buyer/searchCard/SearchIndex.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../../Footer/Footer";
 import Banner from "../../Header/Banner";
 import Navigation from "../../Header/Navigation";
 import CardData from "./components/CardData";
 import Search from "./components/Search";
-import Destination from "../../mainContent/Destination"
+import Destination from "../../mainContent/Destination";
+import {  useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Index = () => {
+  const token = window.localStorage.getItem("accessToken");
+  const history = useNavigate();
+  const { data } = useSelector((state) => state.ticketReducer);
+  useEffect(() => {
+    if (!token) {
+      history("/login");
+    }
+  }, [ history, token]);
   return (
     <>
-      <Navigation />
-      <Banner />
-      <Search />
-      <CardData />
-      <Destination />
-      <Footer />
+      {token && (
+        <>
+          <Navigation />
+          <Banner />
+          <Search />
+          {data && <CardData />}
+          <Destination />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
