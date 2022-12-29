5c0ef7a81555575f75175ac5281d877c8c9de691
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-29 16:07:13 +0700

    add component load data and styling

---
+++ src/components/UI/LoadData.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import classes from "./LoadData.module.css";
+
+const LoadData = () => {
+  return (
+    <div className="text-center">
+      <div className={classes.ldsEllipsis}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadData;
