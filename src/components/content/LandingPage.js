f847e3be92cf6966cb1d2195e885cf0deae4f849
Atha Ardisa <ardisaatha1@gmail.com>
2022-12-30 16:06:20 +0700

    add loading page if navigate to /

--- src/components/content/LandingPage.js
+++ src/components/content/LandingPage.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../Footer/Footer";
 import Banner from "../Header/Banner";
 import Navigation from "../Header/Navigation";
 import Destination from "../mainContent/Destination";
 import Service from "../mainContent/Service";
 import WhyUs from "../mainContent/WhyUs";
+import LoadPage from "../UI/LoadPage";
 
 const LandingPage = () => {
+  const [load, setLoad] = useState(true);
+
+  setInterval(function () {
+    setLoad(false);
+  }, 700);
+
   return (
     <>
-      <Navigation />
-      <Banner />
-      <Service />
-      <Destination />
-      <WhyUs />
-      <Footer />
+      {load ? (
+        <LoadPage />
+      ) : (
+        <>
+          <Navigation />
+          <Banner />
+          <Service />
+          <Destination />
+          <WhyUs />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
