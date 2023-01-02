a3b32e4daff19c4147f17f9c8642dfd2910c3a4d
Atha Ardisa <ardisaatha1@gmail.com>
2023-01-02 21:56:07 +0700

    integrate with API get notif by id and delete notif by id

--- src/services/notifService.js
+++ src/services/notifService.js
@@ -10,6 +10,24 @@ const NotifService = {
       throw error;
     }
   },
+  getNotifId: async function (id) {
+    try {
+      const response = await API.get(`/api/notification/${id}`);
+      return response;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
+  deleteNotifIs: async function (id) {
+    try {
+      const response = await API.delete(`/api/notification/delete/${id}`);
+      return response;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
 };
 
 export default NotifService;
